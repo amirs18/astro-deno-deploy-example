07fbd7000f25521ef818bad1ab19f2c466ecb560
amirs18 <amir135246@gmail.com>
2025-08-24 23:04:28 +0300

    ♻️ refactor(deps): replace lodash with remeda
    - remove lodash
    - add remeda
    - refactor `/src/pages/api/index.ts` to use `remeda` instead of `lodash`

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from "astro";
 import { isNumber } from "remeda"
-export const POST: APIRoute = () => {
+export const GET: APIRoute = () => {
     const int = isNumber("1")
     return new Response(`hello world ${int} `)
 }
